d6fe5d97fc1657ac63290c76ccd9dbab69e18dd9
lourenco-at <82400421+lourenco-at@users.noreply.github.com>
2022-12-20 14:03:22 +0100

    Create react app & Add bootstrap & Add footer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <footer>
+          <p><a href="" target="_blank" >Sourced-code </a>by <a href="" target="_blank">Ana Lourenço</a></p>
+        </footer>
+      </div>
     </div>
   );
 }
 
-export default App;
+
