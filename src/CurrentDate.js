62217e4c9e613ca3fa6f7c7990511c46d70b7560
lourenco-at <82400421+lourenco-at@users.noreply.github.com>
2023-01-09 11:34:41 +0100

    Update CurrentDate.js

--- src/CurrentDate.js
+++ src/CurrentDate.js
@@ -5,7 +5,6 @@ import "./WeatherOverview.css";
 export default function CurrentDate(props){
   console.log(props.date);
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   let month = months[props.date.getMonth()];
   let day = props.date.getDate();
   let hour=props.date.getHours();
